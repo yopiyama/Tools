1bedfe3d867f02cc2c0e165121687e24c2ce34f5
yopiyama <yoshiyama.r24@gmail.com>
2019-01-21 22:38:18 +0900

    implements filter for GrayImgEditor

--- ImgEditor/src/app/App.java
+++ ImgEditor/src/app/App.java
@@ -8,16 +8,19 @@ public class App {
 		String ext = "png";
 
 		String inImgName = "./img/lena.png";
-		String outImgName = "./img/out_ena.png";
+		String outImgName = "./img/out_lena.png";
 		ColorImgEditor sImg = new ColorImgEditor(inImgName);
 		sImg.invertColor();
 		sImg.saveImg(outImgName, ext);
-		new MyFrame(inImgName, sImg.getSize(), inImgName, outImgName);
+		// new MyFrame(inImgName, sImg.getSize(), inImgName, outImgName);
 
 		String inGrayImg = "./img/lena_gray.png";
-		String outGrayImg = "./img/out_ena_gray.png";
+		String outGrayImg = "./img/out_lena_gray.png";
 		GrayImgEditor grayImg = new GrayImgEditor(inGrayImg);
-		grayImg.invertColor();
+		// grayImg.invertColor();
+		// grayImg.setBlurFilter();
+		grayImg.setLaplacianFilter();
+		grayImg.applyFilter();
 		grayImg.saveImg(outGrayImg, ext);
 		new MyFrame(inGrayImg, grayImg.getSize(), inGrayImg, outGrayImg);
 	}
--- ImgEditor/src/util/GrayImgEditor.java
+++ ImgEditor/src/util/GrayImgEditor.java
@@ -5,8 +5,19 @@ package util;
  */
 public class GrayImgEditor extends ImgUtil {
 
+	private double[][] filter = new double[3][3];
+	private int coef = 1;
+	private int offset = 0;
+
 	public GrayImgEditor(String fileName) {
 		super(fileName, ImgUtil.TYPE_GRAY);
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				filter[i][j] = 1.0;
+			}
+		}
+		this.coef = 9;
 	}
 
 	@Override
@@ -20,4 +31,44 @@ public class GrayImgEditor extends ImgUtil {
 			}
 		}
 	}
+
+	public void applyFilter() {
+		double color = 0.0;
+		for (int x = 0; x < this.getSize()[0]; x++) {
+			for (int y = 0; y < this.getSize()[1]; y++) {
+				color = 0.0;
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						int tmpX = x + i;
+						int tmpY = y + j;
+						if (!(tmpX < 0 || tmpY < 0 || tmpX == this.getSize()[0] || tmpY == this.getSize()[1])) {
+							color += filter[i + 1][j + 1] * this.getColor(tmpX, tmpY)[0];
+						}
+					}
+				}
+				color /= this.coef;
+				color += this.offset;
+				if (color > 255) {
+					color = 255.0;
+				} else if (color < 0) {
+					color = 0.0;
+				}
+				// System.out.println(color);
+				this.setColor(x, y, (int) color);
+			}
+		}
+	}
+
+	public void setBlurFilter() {
+		this.filter = new double[][] { { 0.0, 1.0, 0.0 }, { 1.0, 1.0, 1.0 }, { 0.0, 1.0, 0.0 } };
+		this.coef = 5;
+		this.offset = 0;
+	}
+
+	public void setLaplacianFilter() {
+		this.filter = new double[][] { { 0.0, 1.0, 0.0 }, { 1.0, -4.0, 1.0 }, { 0.0, 1.0, 0.0 } };
+		this.coef = 1;
+		this.offset = 0;
+	}
+
 }
